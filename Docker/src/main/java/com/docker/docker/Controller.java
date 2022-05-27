ae7c14b189e8c683627e79d1c32aa9b22c25d30f
Noelia586 <79090242+Noelia586@users.noreply.github.com>
2022-05-27 10:13:00 +0200

    Add files via upload

---
+++ Docker/src/main/java/com/docker/docker/Controller.java
@@ -0,0 +1,21 @@
+package com.docker.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("saludo/{name}")
+    public String saludo(@PathVariable String name) {
+        return "Hola, " + name;
+
+    }
+
+    @GetMapping("adios/{name}")
+    public String adios(@PathVariable String name) {
+        return "Adios, " + name;
+
+    }
+}
